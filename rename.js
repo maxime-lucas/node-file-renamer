fe0aa3862e58fd0b16cb6e5c82f0c02de122b7b5
Maxime LUCAS <maxime.lucas96@gmail.com>
2020-05-27 17:48:53 +0200

    [MLS] Use moment.js for date formating and deal with same-name files

--- rename.js
+++ rename.js
@@ -1,68 +1,94 @@
-var fs = require('fs');
-var exifParser = require('exif-parser');
+var fs = require("fs");
+var exifParser = require("exif-parser");
+var moment = require("moment");
+var path = require("path");
 
-function addZero(data){
-    if(data<10)
-        data = '0'+data;
-    return data;
+function usage() {
+	console.log("rename.js <source_directory> <destination_directory>");
 }
 
-function getCreationDateAsString(fileName){
-    return new Promise((success,failure)=>{
-        fs.readFile(fileName, function(err,data){
-            if(err) throw err;
-            var parser = exifParser.create(data);
-            var exif = parser.parse();
-            var timestamp = exif.tags.DateTimeOriginal;
-            if(!timestamp)
-                return failure(fileName);
-            var date = new Date(timestamp*1000);
-            date.setTime(date.getTime()-3600*1000);
-            var dd = addZero(date.getDate());
-            var mm = addZero(date.getMonth()+1);
-            var yyyy = date.getFullYear();
-            var hh = addZero(date.getHours());
-            var min = addZero(date.getMinutes());
-            var sec = addZero(date.getSeconds());
-            var dateAsString = '';
-            dateAsString += yyyy;
-            dateAsString += mm;
-            dateAsString += dd;
-            dateAsString += hh;
-            dateAsString += min;
-            dateAsString += sec;
-            return success(dateAsString);
-        });
-    })
+function checkArguments(srcDirectory, destDirectory) {
+	if (srcDirectory == null || dstDirectory == null) {
+		usage();
+		return 0;
+	}
+
+	if (!fs.existsSync(srcDirectory)) {
+		console.log("ERROR: Source directory doesn't exist");
+		return 0;
+	}
+
+	if (!fs.existsSync(dstDirectory)) {
+		console.log("ERROR: Destination directory doesn't exist");
+		return 0;
+	}
+
+	if (!fs.lstatSync(srcDirectory).isDirectory()) {
+		console.error("File " + srcDirectory + " is not a directory");
+		return 0;
+	}
+
+	if (!fs.lstatSync(dstDirectory).isDirectory()) {
+		console.error("File " + dstDirectory + " is not a directory");
+		return 0;
+	}
+
+	return 1;
+}
+
+function addZero(data) {
+	if (data < 10) data = "0" + data;
+	return data;
+}
+
+function getCreationDateAsString(fileName) {
+	return new Promise((success, failure) => {
+		fs.readFile(fileName, function (err, data) {
+			if (err) throw err;
+			var parser = exifParser.create(data);
+			var exif = parser.parse();
+			var timestamp = exif.tags.DateTimeOriginal;
+			if (!timestamp) return failure(fileName);
+			let dateAsString = moment.unix(timestamp).utc().format("YYYYMMDD_HHmmss");
+			return success(dateAsString);
+		});
+	});
 }
 
 let myArgs = process.argv.slice(2);
-let directory = myArgs[0];
+let srcDirectory = myArgs[0];
+let dstDirectory = myArgs[1];
 
-if(directory == null){
-    console.log("Missing 'folder' argument")
-    return;
+if (checkArguments(srcDirectory, dstDirectory)) {
+	fs.readdir(srcDirectory, (err, files) => {
+		files.forEach((fileName) => {
+			console.log("Processing file " + fileName);
+			let fileAbsolutePath = srcDirectory + "/" + fileName;
+			let fileExt = path.extname(fileAbsolutePath);
+			if (fileExt == ".jpg") {
+				getCreationDateAsString(fileAbsolutePath)
+					.then((creationDate) => {
+						let fileNewPath = dstDirectory + "/" + creationDate + ".jpg";
+						let i = 1;
+						while (fs.existsSync(fileNewPath)) {
+							fileNewPath =
+								dstDirectory + "/" + creationDate + "_" + i + ".jpg";
+							i++;
+						}
+						fs.copyFile(fileAbsolutePath, fileNewPath, () => {
+							console.log(
+								"File successfully '" +
+									fileAbsolutePath +
+									"' copied to '" +
+									fileNewPath +
+									"'"
+							);
+						});
+					})
+					.catch((fileName) => {
+						console.log("Error when getting creation date of file " + fileName);
+					});
+			}
+		});
+	});
 }
-
-fs.access(directory, fs.F_OK, (err)=>{
-    if(err) {
-        console.error(err)
-        return;
-    }
-    if(!fs.lstatSync(directory).isDirectory()) {
-        console.error("File "+directory+" is not a directory");
-        return;
-    }
-    fs.readdir(directory,(err,files)=>{
-        files.forEach(fileName=>{
-            console.log("Processing file "+fileName);
-            let fileFullPath = directory+"/"+fileName;
-            getCreationDateAsString(fileFullPath).then((creationDate)=>{
-                let fileNewPath = directory+"/"+creationDate+".jpg";
-               fs.renameSync(fileFullPath,fileNewPath);
-            }).catch((fileName)=>{
-                console.log("Error when getting creation date of file "+fileName);
-            })
-        })
-    })
-})
